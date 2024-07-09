35720c146d76cf9958cb27110de7af8194c56359
PrideKetu <ketupride1@gmail.com>
2024-07-09 18:53:45 +0100

    NEW PUSH

---
+++ BLOOD_BANKING_SYSTEM-MobileEnd/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BLOOD_BANKING_SYSTEM-MobileEnd',
+  webDir: 'www'
+};
+
+export default config;
