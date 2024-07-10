91dced221ca5a1999d149f5d7379489d5df1a479
Hansel681 <atudhansel3@gmail.com>
2024-07-10 19:46:16 +0100

    dev branch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const concurrently = require('concurrently');
 
 const commands = [
 //{ command: 'cd BLOOD_BANKING_SYSTEM-MobileEnd && npx ionic serve', name: 'Ionic', prefixColor: 'blue' },
-{ command: 'cd BLOOD_BANKING_SYSTEM-webEnd && npx serve -s .', name: 'HTML5', prefixColor: 'green' }
+{ command: 'cd BLOOD_BANKING_SYSTEM-webEnd && npx serve .', name: 'HTML5', prefixColor: 'green' }
 ];
 
 concurrently(commands, {
