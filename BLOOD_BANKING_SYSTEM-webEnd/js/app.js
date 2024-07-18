7f63a5d2f8b2891de5217bcfaf497eaa294b8b85
Hansel681 <atudhansel3@gmail.com>
2024-07-18 19:57:16 +0100

    new

--- BLOOD_BANKING_SYSTEM-webEnd/js/app.js
+++ BLOOD_BANKING_SYSTEM-webEnd/js/app.js
@@ -5,7 +5,8 @@ async function includeHTML(elementId, file) {
 }
 
 includeHTML('hansel','/pages/hansel_page.html');
-includeHTML('kelly','/pages/kelly_page.html');
+includeHTML('kelly', '/pages/kelly_page.html');
+includeHTML('kelly','/pages/hilford_page.html');
 includeHTML('pasky','/pages/pasky_page.html');
 includeHTML('pride','/pages/pride_page.html');
 
