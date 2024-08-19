90b947ef94accfa836a55a2f7d11c54519991473
PrideKetu <ketupride1@gmail.com>
2024-08-19 17:15:02 +0100

    backend updates

--- BLOOD_BANKING_SYSTEM-BackEnd/server.js
+++ BLOOD_BANKING_SYSTEM-BackEnd/server.js
@@ -1,11 +1,15 @@
 const bodyparser = require('body-parser');
 const express = require('express')
 const hosp_route = require('./src/routes/hospital_route')
+const don_route = require('./src/routes/donor_route')
+const cli_route = require('./src/routes/client_route')
 const app = express()
 const port = 8000
 app.use(bodyparser.json());
 
 app.use('/hospital', hosp_route);
+app.use('/donor' , don_route);
+app.use('/client' , cli_route);
 
   app.listen(port, () => {
     console.log(`Example app listening on port http://localhost${port}`)
