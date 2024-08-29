f94afbedbc51f5f02c5c35f7f9f9c120ddeb3424
PrideKetu <ketupride1@gmail.com>
2024-08-29 01:06:46 +0100

    new page

--- BLOOD_BANKING_SYSTEM-webEnd/js/app.js
+++ BLOOD_BANKING_SYSTEM-webEnd/js/app.js
@@ -5,9 +5,8 @@ async function includeHTML(elementId, file) {
 }
 
 includeHTML('hansel','/pages/hansel_page.html');
-includeHTML('acha', '/pages/achah_page.html');
 includeHTML('kelly', '/pages/kelly_page.html');
 includeHTML('hilford','/pages/hilford_page.html');
 includeHTML('pasky','/pages/pasky_page.html');
 includeHTML('pride','/pages/pride_page.html');
-
+includeHTML('achah', '/pages/achah_page.html');
--- BLOOD_BANKING_SYSTEM-webEnd/js/dashboard/dashboard.js
+++ BLOOD_BANKING_SYSTEM-webEnd/js/dashboard/dashboard.js
@@ -1,27 +1,58 @@
 document.getElementById('modeToggle').addEventListener('change', function() {
     document.body.classList.toggle('dark-mode');
 });
+    document.addEventListener('DOMContentLoaded', () => {
+        const links = document.querySelectorAll('sidebar ul li');
+        const maincontent = document.querySelector('maincontent')
+    });
+
+    links.forEach(links => {
+       links.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const section = link.getAttribute('id')
+
+        links.forEach(item => item.classList.remove('active'));
+        
+        link.classList.add('active');
+        loadSection(section);
+       }) 
+    });
+
+
+    function loadSection(section) {
+        fetch(`dashboard/${section}.html`)
+          .then(response => response.text())
+          .then(html => {
+            mainContent.innerHTML = html;
+          })
+          .catch(error => {
+            console.error('server loading section:',error);
+          });
+        }
+
 
 const ctx1 = document.getElementById('patientStatistic').getContext('2d');
 
 
-const patientStatisticChart = new Chart(ctx1, {
-    type: 'bar',
-    data: {
-        labels: ['Hospital A', 'Hospital B', 'Hospital C', 'Hospital D'],
-        datasets: [{
-            label: 'Patients',
-            data: [12, 19, 3, 5],
-            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
-            borderColor: ['rgba(75, 192, 192, 1)'],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+
+// const patientStatisticChart = new Chart(ctx1, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Hospital A', 'Hospital B', 'Hospital C', 'Hospital D'],
+//         datasets: [{
+//             label: 'Patients',
+//             data: [12, 19, 3, 5],
+//             backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+//             borderColor: ['rgba(75, 192, 192, 1)'],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+        //}
+  //  }
+//});
