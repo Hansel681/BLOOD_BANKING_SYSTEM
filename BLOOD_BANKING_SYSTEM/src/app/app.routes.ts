6aa2baf57ed0e8dbd5cf162efa9ae8b4949f715d
noubissie pavel <noubissiepavel@gmail.com>
2024-09-05 18:09:19 +0100

    [merge] - dashboard

--- BLOOD_BANKING_SYSTEM/src/app/app.routes.ts
+++ BLOOD_BANKING_SYSTEM/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import {SignInComponent} from "./features/sign-in/sign-in.component";
 export const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home component'},
   {path: 'sign-up', component: SignUpComponent, title: 'Sign-Up component'},
-  {path: 'sign-in', component:SignInComponent, title: 'Sign-In component'},
+  {path: 'sign-in', component: SignInComponent, title: 'Sign-In component'},
   {
     path: 'dashboard',
     loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.routes)
